880d0b50f76a808fc6c8d1de7d107b7f3f7977f5
BioBabe2002 <150148922+BioBabe2002@users.noreply.github.com>
2023-12-06 23:55:00 +0000

    Add files via upload

---
+++ Q5 Code to produce figure.R
@@ -0,0 +1,12 @@
+library(readr)
+library(ggplot2)
+
+Cui_etal2014 <- read.csv("question-5-data/Cui_etal2014.csv")
+
+plot1 <- ggplot(data = Cui_etal2014, aes(x = log(Genome.length..kb.), y = log(Virion.volume..nm.nm.nm.))) +
+  
+  geom_point() +
+  geom_smooth(method = lm) +
+  theme_bw() +
+  labs(x = 'Log[Genome length (kb)]', y = 'Log[Virion volume (nm^3)]') 
+plot1
